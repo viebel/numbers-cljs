148bf51aecb1e3cce616de8dcfe6569cd6432239
Yehonathan Sharvit <viebel@gmail.com>
2014-01-14 14:52:42 +0200

    skeleton

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject modern-cljs "0.1.0-SNAPSHOT"
-  :description "A series of tutorials on ClojureScript"
-  :url "https://github.com/magomimmo/modern-cljs" ;https://github.com/magomimmo/modern-cljs/blob/master/doc/tutorial-18.md
+(defproject numbers-cljs "0.1.0-SNAPSHOT"
+  :description "Play with numbers via ClojureScript and Angular.js"
+  :url "" ;https://github.com/magomimmo/modern-cljs/blob/master/doc/tutorial-18.md
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
@@ -14,107 +14,35 @@
   :min-lein-version "2.2.0"
 
   ;; clojure source code path
-  :source-paths ["src/clj" "src/cljs" "src/brepl"]
-  :test-paths ["target/test/clj" "target/test/cljs"]
+  :source-paths ["src/clj" "src/cljs"]
 
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/clojurescript "0.0-2069"]
-                 [com.cemerick/piggieback "0.1.2"]
                  [compojure "1.1.6"]
-                 [hiccups "0.2.0"]
-                 [domina "1.0.3-SNAPSHOT"]
-                 [org.clojars.magomimmo/shoreleave-remote-ring "0.3.1-SNAPSHOT"]
-                 [org.clojars.magomimmo/shoreleave-remote "0.3.1-SNAPSHOT"]
-                 [com.cemerick/valip "0.3.2"]
-                 [enlive "1.1.4"]]
+                 ]
 
   :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
-  :injections [(require '[cljs.repl.browser :as brepl]
-                        '[cemerick.piggieback :as pb])
-               (defn browser-repl []
-                 (pb/cljs-repl :repl-env (brepl/repl-env :port 9000)))]
-
   :plugins [[lein-cljsbuild "1.0.0"]
-            [lein-ring "0.8.8"]
-            [com.keminglabs/cljx "0.3.0"]
-            [com.cemerick/clojurescript.test "0.2.1"]]
+            [lein-ring "0.8.8"] ]
 
   :hooks [leiningen.cljsbuild]
 
-  :cljx {:builds [{:source-paths ["test/cljx"]
-                   :output-path "target/test/clj"
-                   :rules :clj}
+  ;; ring tasks configuration
+  :ring {:handler numbers-cljs.core/app}
 
-                  {:source-paths ["test/cljx"]
-                   :output-path "target/test/cljs"
-                   :rules :cljs}]}
-
-  :ring {:handler modern-cljs.core/app}
-
-  :cljsbuild {:crossovers [valip.core
-                           valip.predicates
-                           modern-cljs.login.validators
-                           modern-cljs.shopping.validators]
-              ;; for unit testing with phantomjs
-              :test-commands {"phantomjs-whitespace"
-                              ["phantomjs" :runner "target/test/js/testable_dbg.js"]
-
-                              "phantomjs-simple"
-                              ["phantomjs" :runner "target/test/js/testable_pre.js"]
-
-                              "phantomjs-advanced"
-                              ["phantomjs" :runner "target/test/js/testable.js"]}
-              :builds
-              {:ws-unit-tests
-               { ;; clojurescript source code path
-                :source-paths ["src/brepl" "src/cljs" "target/test/cljs"]
-
-                ;; Google Closure Compiler options
-                :compiler { ;; the name of emitted JS script file
-                           :output-to "target/test/js/testable_dbg.js"
-
-                           ;; minimum optimization
-                           :optimizations :whitespace
-                           ;; prettyfying emitted JS
-                           :pretty-print true}}
-
-               :simple-unit-tests
-               { ;; same path as above
-                :source-paths ["src/brepl" "src/cljs" "target/test/cljs"]
-
-                :compiler { ;; different JS output name
-                           :output-to "target/test/js/testable_pre.js"
-
-                           ;; simple optimization
-                           :optimizations :simple
-
-                           ;; no need prettification
-                           :pretty-print false}}
-
-               :advanced-unit-tests
-               { ;; same path as above
-                :source-paths ["src/cljs" "target/test/cljs"]
-
-                :compiler { ;; different JS output name
-                           :output-to "target/test/js/testable.js"
-
-                           ;; advanced optimization
-                           :optimizations :advanced
-
-                           ;; no need prettification
-                           :pretty-print false}}
-
+  ;; cljsbuild tasks configuration
+  :cljsbuild {:builds
+              {
                :dev
                { ;; clojurescript source code path
-                :source-paths ["src/brepl" "src/cljs"]
+                :source-paths ["src/cljs"]
 
                 ;; Google Closure Compiler options
                 :compiler { ;; the name of emitted JS script file
-                           :output-to "resources/public/js/modern.js"
-
+                           :output-to "resources/public/js/main.js"
                            ;; minimum optimization
                            :optimizations :whitespace
                            ;; prettyfying emitted JS
                            :pretty-print true}}
                }}
-               )
+   )
--- src/clj/numbers_cljs/core.clj
+++ src/clj/numbers_cljs/core.clj
@@ -1,10 +1,8 @@
-(ns modern-cljs.core
+(ns numbers-cljs.core
   (:require [compojure.core :refer [defroutes GET POST]]
             [compojure.route :refer [resources not-found]]
             [compojure.handler :refer [site]]
-            [modern-cljs.login :refer [authenticate-user]]
-            [modern-cljs.templates.shopping :refer [shopping]]
-            [shoreleave.middleware.rpc :refer [wrap-rpc]]))
+            ))
 
 ;; defroutes macro defines a function that chains individual route
 ;; functions together. The request map is passed to each function in
@@ -12,11 +10,6 @@
 (defroutes app-routes
   ;; to serve document root address
   (GET "/" [] "<p>Hello from compojure</p>")
-  ;; to authenticate the user
-  (POST "/login" [email password] (authenticate-user email password))
-  ;; to server shopping command
-  (POST "/shopping" [quantity price tax discount]
-        (shopping quantity price tax discount))
   ;; to server static pages saved in resources/public directory
   (resources "/")
   ;; if page is not found
@@ -27,7 +20,5 @@
 (def handler
   (site app-routes))
 
-(def app (-> (var handler)
-             (wrap-rpc)
-             (site)))
+(def app (-> (var handler)))
 
