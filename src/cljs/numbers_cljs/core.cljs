572707451cf6af489c721d1b7d1df45f8e79a199
Yehonathan Sharvit <viebel@gmail.com>
2014-01-19 22:49:45 +0200

    use merge

--- src/cljs/numbers_cljs/core.cljs
+++ src/cljs/numbers_cljs/core.cljs
@@ -1,5 +1,5 @@
 (ns numbers_cljs.core
-  (:require [cljs.reader :as reader]))
+  (:require [clojure.string :as string]))
 
 ;; angular helpers
 (defn- ng-route [provider path route-spec]
@@ -20,25 +20,30 @@
 
 (defn- numbers-ctrl [$scope]
   (let [
-       model-defaults (fn[]
-          (doto $scope
-            (aset "range" 100)
-            (aset "divisor" 2)
-            (aset "condition" "x % 4 == 0")))
+       my-merge (fn[a b]
+          (doseq [[k v] b]
+            (aset a k v)))
        watch-changes (fn[]
             (.$watchCollection $scope "[range, divisor, condition]" (
                fn[[rr dd condition] old-vals] 
                 (let [[my-range divisor] (map #(js/parseInt %) [rr dd])
                       numbers (range 1 (+ 1 my-range))
+                      check (fn[z] (try 
+                                     (js/eval (string/replace condition "x" z))
+                                     (catch js/Object e  false)))
                       numbers-with-info (map (fn[x] { :n x
                                                       :divisible (= 0 (mod x divisor))
-                                                      :condition false}) 
+                                                      :condition (check x)
+                                                    }) 
                                          numbers)]
-                (aset $scope "numbers" (clj->js numbers))
-                (aset $scope "numbersWithInfo" (clj->js numbers-with-info))))))
+                (doto $scope 
+                  (aset "numbers" (clj->js numbers))
+                  (aset "numbersWithInfo" (clj->js numbers-with-info)))))))
         ]
 
-  (model-defaults)
+  (my-merge $scope { "range" 100
+                     "divisor" 2
+                     "condition" "x % 4 == 0"})
   (watch-changes)))
 
 ;; main
