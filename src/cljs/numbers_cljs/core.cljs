09d0358ea27bb06461f0a1653f4f61b549fa0a13
Yehonathan Sharvit <viebel@gmail.com>
2014-01-19 22:01:42 +0200

    shorter code

--- src/cljs/numbers_cljs/core.cljs
+++ src/cljs/numbers_cljs/core.cljs
@@ -26,9 +26,8 @@
           (aset $scope "condition" "x % 4 == 0"))
        watch-changes (fn[]
             (.$watchCollection $scope "[range, divisor, condition]" (
-               fn[new-vals] 
-                (let [[rr dd condition] new-vals
-                      [my-range divisor] (map #(js/parseInt %) [rr dd])
+               fn[[rr dd condition] old-vals] 
+                (let [[my-range divisor] (map #(js/parseInt %) [rr dd])
                       numbers (range 1 (+ 1 my-range))
                       numbers-with-info (map (fn[x] { :n x
                                                       :divisible (= 0 (mod x divisor))
